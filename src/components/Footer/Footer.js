985f75e4f87b95b13b1b5bda7625988a7215363e
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2022-07-21 15:02:40 +0300

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer" >
             <div id="footer-text" >
-                2022 All copyrights reserved: &copy;
+                2022 All copyrights reserved &copy;
             </div>
         </footer>
     )
