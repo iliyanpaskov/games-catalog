4f768b3d8cc9ebbefb507af2cf5430d47dd34dc4
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2022-07-28 19:16:29 +0300

    my-games card fixed

--- src/components/MyGames/MyGamesCard/MyGamesCard.js
+++ src/components/MyGames/MyGamesCard/MyGamesCard.js
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
+import "./MyGamesCard.css"
 
 const MyGamesCard = ({
     game
 }) => {
 
     return (
-        <div className="top-games-card">
-            <img src={game.imageUrl} className="card-img-top" alt="Not found..." />
-            <div className="card-body">
-                <h3 className="card-title"> {game.title}</h3>
-                <p className="card-text">Category: {game.category}</p>
-                {/* TODO  Links..Edit  */}
-                <ul className="cards-btns-wrapper">
+        <div className="my-games-card">
+            <img src={game.imageUrl} className="my-card-img" alt="Not found..." />
+            <div className="my-card-body">
+                <h3 className="my-card-title"> {game.title}</h3>
+                <p className="my-card-text">Category: {game.category}</p>
+                {/* TODO  Links..Edit and Comment */}
+                <ul className="my-cards-btns-wrapper">
                     <li>
-                    <Link to={`/data/games/${game._id}`} className="top-games-card-btn" >Details</Link>
+                    <Link to={`/data/games/${game._id}`} className="my-games-card-btn" >Details</Link>
                     </li>
                     <li>
-                        <Link to="#" className="top-games-card-btn" >Edit</Link>
+                        <Link to="#" className="my-games-card-btn" >Edit</Link>
                     </li>
 
                 </ul>
